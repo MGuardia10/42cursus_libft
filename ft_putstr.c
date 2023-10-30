76e1c38ad887f1cd048ea88cfa863001a7c42680
Miguel Guardia Sanchez <mguardia@mguardia.local>
2023-10-30 20:35:48 +0100

    documentation added

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,12 +6,20 @@
 /*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 13:04:08 by mguardia          #+#    #+#             */
-/*   Updated: 2023/10/30 20:19:42 by mguardia         ###   ########.fr       */
+/*   Updated: 2023/10/30 20:35:12 by mguardia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * The ft_putstr function takes a string as input and outputs it to the console, returning the number
+ * of characters printed.
+ * 
+ * @param str A pointer to a character array (string) that we want to output.
+ * 
+ * @return the number of characters printed.
+ */
 int	ft_putstr(char *str)
 {
 	int	count;
