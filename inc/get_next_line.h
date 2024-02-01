01badf1c40ac90e87efc87d5711b7ff71fea564c
Miguel Guardia <mike.guardia0309@gmail.com>
2024-02-01 16:20:51 +0100

    colors header, printf header, gnl header and keycodes header added

---
+++ inc/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 09:37:10 by mguardia          #+#    #+#             */
+/*   Updated: 2024/02/01 16:16:30 by mguardia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char		*get_next_line(int fd);
+char		*ft_strchr(const char *s, int c);
+size_t		ft_strlen(const char *s);
+
+#endif
---
+++ inc/mouse_keycodes.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mouse_keycodes.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mguardia <mguardia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 09:36:56 by mguardia          #+#    #+#             */
+/*   Updated: 2024/02/01 09:36:58 by mguardia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MOUSE_KEYCODES_H
+# define MOUSE_KEYCODES_H
+
+# define LEFT_CLICK		1
+# define RIGHT_CLICK	2
+# define MIDDLE_CLICK	3
+# define SCROLL_UP		4
+# define SCROLL_DOWN	5
+
+#endif
